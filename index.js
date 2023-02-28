76b3bf5dfed16802707d2110dbee13de8cacc2f7
kamrul-7 <kamrul7.khan@gmail.com>
2023-02-28 23:48:02 +0600

    final

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { query } = require('express');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -22,6 +23,7 @@ client.connect(err => {
 async function run() {
     try {
         const PlaceCollection = client.db('Triptastic').collection('Place');
+        const commentCollection = client.db('Triptastic').collection('comment');
 
         app.get('/Place', async (req, res) => {
             const query = {};
@@ -35,6 +37,37 @@ async function run() {
             const service = await PlaceCollection.findOne(query);
             res.send(service);
         });
+
+        app.post("/place", async (req, res) => {
+            const filter = req.body;
+            const result = await PlaceCollection.insertOne(filter);
+            res.send(result);
+        });
+
+
+        app.post('/comment', async (req, res) => {
+            const comment = req.body;
+            const query = {
+                userName: comment.name, comment: comment.comment, email: comment.email
+            };
+            const result = await commentCollection.insertOne(comment);
+            res.send(result);
+        })
+
+
+        app.get('/comment', async (req, res) => {
+            const query = {};
+            const options = await commentCollection.find(query).toArray();
+            res.send(options);
+        });
+
+        app.get('/comment', async (req, res) => {
+            const email = req.query.email;
+            const qury = { email: email };
+            const comment = await commentCollection.find(query).toArray();
+            res.send(comment);
+        });
+
     }
     finally {
 
